099e2f2d7837179589c0adf768045607a3ac4c30
Hikmat Rajabli <iamrajabli@Hikmats-MacBook-Air.local>
2025-03-05 12:54:06 +0400

    [event name]

--- src/components/EventButton.tsx
+++ src/components/EventButton.tsx
@@ -5,7 +5,7 @@ export function EventButton() {
     <div>
       <button
         className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded cursor-pointer'
-        onClick={() => sendGTMEvent({ event: 'buttonClicked', value: 'xyz' })}
+        onClick={() => sendGTMEvent({ event: 'button_clicked', value: 'xyz' })}
       >
         Send Event
       </button>
