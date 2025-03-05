37ca9d409cba3c8eaee18e86435cbc26206304d9
Hikmat Rajabli <iamrajabli@Hikmats-MacBook-Air.local>
2025-03-05 14:15:34 +0400

    [events]

--- src/components/EventButton.tsx
+++ src/components/EventButton.tsx
@@ -2,12 +2,19 @@ import { sendGTMEvent } from '@next/third-parties/google'
  
 export function EventButton() {
   return (
-    <div>
+    <div className='flex gap-4'>
       <button
         className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded cursor-pointer'
-        onClick={() => sendGTMEvent({ event: 'button_clicked', value: 'xyz' })}
+        onClick={() => sendGTMEvent({ event: 'button_clicked', value: 'xyz',  })}
       >
         Send Event
+      </button>
+
+      <button
+        className='bg-blue-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded cursor-pointer'
+        onClick={() => sendGTMEvent({ event: 'fn_clicked', value: 'xyz',  })}
+      >
+        Send FN
       </button>
     </div>
   )
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,3 +1,4 @@
+import { GoogleAnalytics, GoogleTagManager } from "@next/third-parties/google";
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
@@ -7,6 +8,12 @@ export default function Document() {
       <body className="antialiased">
         <Main />
         <NextScript />
+
+        <GoogleTagManager gtmId="GTM-5S4667XS" />
+        <GoogleAnalytics gaId="G-KMM12Q8YPC" />
+
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5S4667XS"
+          height="0" width="0" style={{display:"none",visibility:"hidden"}}></iframe></noscript>
       </body>
     </Html>
   );
