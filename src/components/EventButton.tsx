249194890477f777d4e8806eaa726b61e6f45a0d
Hikmat Rajabli <iamrajabli@Hikmats-MacBook-Air.local>
2025-03-05 11:23:05 +0400

    [google tag manager]

--- src/components/EventButton.tsx
+++ src/components/EventButton.tsx
@@ -4,7 +4,7 @@ export function EventButton() {
   return (
     <div>
       <button
-      className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded cursor-pointer'
+        className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded cursor-pointer'
         onClick={() => sendGTMEvent({ event: 'buttonClicked', value: 'xyz' })}
       >
         Send Event
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,12 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { GoogleTagManager } from '@next/third-parties/google'
+import { GoogleAnalytics, GoogleTagManager } from '@next/third-parties/google'
 
 
 export default function App({ Component, pageProps }: AppProps) {
   return <>
     <Component {...pageProps} />
     <GoogleTagManager gtmId="GTM-5S4667XS" />
+    <GoogleAnalytics gaId="G-D0833WSE5N" />
   </>
 }
